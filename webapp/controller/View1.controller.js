4037850357eff4e4d7d83f7e289e5c4021d9e4b6
MaxoGitAcc <maxowork19@gmail.com>
2025-11-12 23:14:10 +0400

    Displayed table on page

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,10 +1,14 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
+], (Controller, JSONModel) => {
     "use strict";
 
     return Controller.extend("project1.controller.View1", {
         onInit() {
+            var oBookModel = new JSONModel(jQuery.sap.getModulePath("project1.model.books", ".json"));
+
+            this.getView().setModel(oBookModel, "bookModel1");
         }
     });
 });
