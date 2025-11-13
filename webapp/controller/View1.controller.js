d371ae95daa812d25fbc1ab095b6fbdd83a23635
MaxoGitAcc <maxowork19@gmail.com>
2025-11-13 13:00:16 +0400

    Search bar

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -9,6 +9,24 @@ sap.ui.define([
             var oBookModel = new JSONModel(jQuery.sap.getModulePath("project1.model.books", ".json"));
 
             this.getView().setModel(oBookModel, "bookModel1");
+        },
+        
+        //Search
+        onSearch: function(oEvent) {
+            var sQuery = oEvent.getParameter("query");
+            var oTable = this.byId("bookTable");
+            var oBinding = oTable.getBinding("items");
+
+            if(sQuery) {
+                var oFilter = new sap.ui.model.Filter("Name", sap.ui.model.FilterOperator.Contains, sQuery);
+                oBinding.filter([oFilter]);
+            } else {
+                oBinding.filter([])
+            }
+            console.log("Search triggered:", sQuery);
+            console.log("Table:", oTable);
+            console.log("Binding:", oBinding);
+
         }
     });
 });
