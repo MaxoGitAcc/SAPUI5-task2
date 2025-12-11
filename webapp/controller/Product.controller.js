f57a5d7f8e08f994f4426e4aa183100220eafd7a
MaxoGitAcc <maxowork19@gmail.com>
2025-12-11 15:51:07 +0400

    sections

--- webapp/controller/Product.controller.js
+++ webapp/controller/Product.controller.js
@@ -3,5 +3,25 @@ sap.ui.define([
 ],(BaseController) => {
     "use strict";
 
-    return BaseController.extend("project1.controller.Product",{})
+    return BaseController.extend("project1.controller.Product",{
+        onInit: function () {
+            const oRouter = this.getOwnerComponent().getRouter();
+            oRouter.getRoute("Product").attachPatternMatched(this._onObjectMatched, this);
+        },
+
+        _onObjectMatched: function (oEvent) {
+            const sProductId = oEvent.getParameter("arguments").ProductID;
+            const sPath = "/Products(" + sProductId + ")";
+        
+            this.getView().bindElement({
+                path: sPath,
+                model: "oDataV2Model",
+                parameters: {
+                    expand: "Supplier"
+                }
+            });
+            
+        }
+        
+    })
 })
