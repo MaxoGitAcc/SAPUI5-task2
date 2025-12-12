277fadee8d10e58d27b5cbf62cdf70cd68be64cf
MaxoGitAcc <maxowork19@gmail.com>
2025-12-12 15:57:16 +0400

    table update

--- webapp/controller/Product.controller.js
+++ webapp/controller/Product.controller.js
@@ -1,7 +1,6 @@
 sap.ui.define([
-    "project1/controller/BaseController",
-    "sap/ui/model/json/JSONModel"
-],(BaseController, JSONModel) => {
+    "project1/controller/BaseController"
+],(BaseController) => {
     "use strict";
 
     return BaseController.extend("project1.controller.Product",{
@@ -9,32 +8,19 @@ sap.ui.define([
             const oRouter = this.getOwnerComponent().getRouter();
             oRouter.getRoute("Product").attachPatternMatched(this._onObjectMatched, this);
         },
+        
 
         _onObjectMatched: function (oEvent) {
             const sProductId = oEvent.getParameter("arguments").ProductID;
             const sPath = "/Products(" + sProductId + ")";
-            const oView = this.getView();
-
-            oView.setModel(new JSONModel({ Suppliers: [] }), "supplierModel");
         
-            oView.bindElement({
+            this.getView().bindElement({
                 path: sPath,
                 model: "oDataV2Model",
                 parameters: {
                     expand: "Supplier"
-                },
-                events: {
-                    dataReceived: function () {
-                        const oSupplier = oView.getBindingContext("oDataV2Model").getProperty("Supplier");
-        
-                        const oSupplierModel = new JSONModel({
-                            Suppliers: [oSupplier]
-                        });
-        
-                        oView.setModel(oSupplierModel, "supplierModelV2");
-                    }
                 }
             });
-        }
+        }        
     })
 })
